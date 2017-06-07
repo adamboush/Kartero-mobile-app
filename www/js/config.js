672ac73dc8e3c74041aff713653644bd1fea2a99
joseaurorah <josedeororah@gmail.com>
2017-06-07 12:10:18 +0300

    kateroapp
    kateroapp

---
+++ www/js/config.js
@@ -0,0 +1,7 @@
+/*Kartero App Configuration*/
+var krms_driver_config ={		
+	'ApiUrl':"http://192.168.0.35/kartero/api",					
+	'DialogDefaultTitle':"Kartero",	
+	'PushProjectID':"1006370283875",	
+	'APIHasKey':"AIzaSyCCqKj53JxccxKBFeJJy7XdZHVzxgc2RJU"
+};
